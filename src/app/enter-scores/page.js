9f14549de51dfad2fec73a82c0e3f7f4c50ff8a5
dayofthetech <j.alberto.reyes.r90@gmail.com>
2024-05-03 15:13:42 -0400

    create two columns, one for category and one for text box.

--- src/app/enter-scores/page.js
+++ src/app/enter-scores/page.js
@@ -10,49 +10,49 @@ export default function enterScores() {
           score report, choose a Job Zone, and view and print careers matching
           your profile.
         </p>
-        <div className="grid grid-cols-1 gap-4 text-center">
-          <div>
-            <label htmlFor="score1">Word 1</label>
+        <div>
+          <div className="grid grid-cols-2 ">
+            <label htmlFor="score1">Realistic</label>
             <input
               type="text"
               id="score1"
               className="border border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"
             />
           </div>
-          <div>
-            <label htmlFor="score2">Word 2</label>
+          <div className="grid grid-cols-2 gap-2-4-2">
+            <label htmlFor="score2">Investigative</label>
             <input
               type="text"
               id="score2"
               className="border border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"
             />
           </div>
-          <div>
-            <label htmlFor="score3">Word 3</label>
+          <div className="grid grid-cols-2 gap-2-4-2">
+            <label htmlFor="score3">Artistic</label>
             <input
               type="text"
               id="score3"
               className="border border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"
             />
           </div>
-          <div>
-            <label htmlFor="score4">Word 4</label>
+          <div className="grid grid-cols-2 gap-2-4-2">
+            <label htmlFor="score4">Social</label>
             <input
               type="text"
               id="score4"
               className="border border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"
             />
           </div>
-          <div>
-            <label htmlFor="score5">Word 5</label>
+          <div className="grid grid-cols-2 gap-2-4-2">
+            <label htmlFor="score5">Enterprising</label>
             <input
               type="text"
               id="score5"
               className="border border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"
             />
           </div>
-          <div>
-            <label htmlFor="score6">Word 6</label>
+          <div className="grid grid-cols-2 gap-2-4-2">
+            <label htmlFor="score6">Conventional</label>
             <input
               type="text"
               id="score6"
