c8e1672a5e1ce1dfc88bebc538efabc7837b8a19
Marcia <hmarcia40@gmail.com>
2024-05-07 17:55:03 -0400

    nav bar added, not functional yet

--- src/app/assessment/results/investigative/page.js
+++ src/app/assessment/results/investigative/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function investigative() {
     return (
         <div className="ml-52 mr-52">
-        <h1 className="text-6xl mt-5 mb-14 flex justify-center title text-wrap">Artistic Interest Information</h1>
+        <h1 className="text-6xl mt-5 mb-14 flex justify-center title text-wrap">Investigative Interest Information</h1>
         <h1 className="text-xl mb-5 "> People with Investigative interests like work that has to do with ideas and thinking rather than physical activity or leading people. </h1>
         <h1 className="text-xl mb-5 ">They like:</h1>
         <h1 className="text-xl m-5">Searching for facts <br />
--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,10 +1,86 @@
+"use client";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 import Link from "next/link";
 
-//routes are named in alphabetical order to make them easy to search for now. Will update
-//when we know what each route will be for
 export default function BottomNav() {
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+  
+    const handleMenu = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
     return (
         <>
+
+<Box sx={{ flexGrow: 1 }}>
+      <AppBar className="navBar" position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            
+            <MenuIcon />
+          </IconButton>
+          
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Career Spring
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+
+
+
+
         <h1>Click the number underneath the circle to test out routes.</h1>
             <div className="overflow-x-auto">
                 <ul className="steps">
