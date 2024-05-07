6feb47a9e0fa6699e90d714f0f4341ad6b06ec62
Marcia <hmarcia40@gmail.com>
2024-05-07 17:19:44 -0400

    layout placed on top of page, job zone 3 investigative given its own nested route in job zone 3 route, investigative info updated

--- src/app/assessment/results/investigative/page.js
+++ src/app/assessment/results/investigative/page.js
@@ -2,17 +2,12 @@ import Link from "next/link";
 export default function investigative() {
     return (
         <div className="ml-52 mr-52">
-            <h1 className="text-6xl mt-5 flex justify-center title text-wrap text-center">All Investigative Careers Job Zone 3</h1>
-            <h1 className="text-xl m-3">Click to change your preferred job zone: </h1>
-            <div className="flex flex-row justify-center">
-                <button className="orangeBrand grid grid-col mr-10 font-bold"><Link href="/">3</Link></button>
-                <button className="orangeBrand grid grid-col ml-10 mr-10 font-bold"><Link href="/">4</Link></button>
-                <button className="orangeBrand flex flex-row ml-10 mr-10 font-bold"><Link href="/">5</Link></button>
-            </div>
-            <h1 className="text-xl mt-10">Investigative: (Score) </h1>
-            <h1 className="text-xl mt-10"> Investigative careers that fit your preparation level:</h1>
-
-            <button className="blueButtons flex justify-center"><Link href="/">Back</Link></button>
-        </div>
+        <h1 className="text-6xl mt-5 mb-14 flex justify-center title text-wrap">Artistic Interest Information</h1>
+        <h1 className="text-xl mb-5 "> People with Investigative interests like work that has to do with ideas and thinking rather than physical activity or leading people. </h1>
+        <h1 className="text-xl mb-5 ">They like:</h1>
+        <h1 className="text-xl m-5">Searching for facts <br />
+        Figuring out problems</h1>
+        <button className="blueButtons flex justify-center"><Link href="/">Back</Link></button>
+    </div>
     )
 }
