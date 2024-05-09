97be320eebeac5cc6f56ebb1c2cd0fd7ec5fe977
davonbl-bl <davonbl@gmail.com>
2024-05-09 15:30:43 -0400

    reserve the parent-child relationship from button-link to link-button on the QuizButtons component

--- src/components/QuizButtons.js
+++ src/components/QuizButtons.js
@@ -4,12 +4,16 @@ import Link from "next/link";
 export default function QuizButtons({back, next}){
     return (
         <section className='flex justify-around align-center items-center py-5'>
-            <button className="bg-sky-600 w-2/12 h-14 rounded p-2.5">
-                <Link href={back}>Back</Link>
-            </button>
-            <button className="bg-sky-600 w-2/12 h-14 rounded p-2.5">
-                <Link href={next}>Next</Link>
-            </button>
+            <Link href={back} className="bg-sky-600 w-2/12 h-14 rounded p-2.5">
+                <button>
+                    Back
+                </button>
+            </Link>
+            <Link href={next} className="bg-sky-600 w-2/12 h-14 rounded p-2.5">
+                <button>
+                    Next
+                </button>
+            </Link>
         </section>
        
     )
