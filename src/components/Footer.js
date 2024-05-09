e569b0ed5b869665bca983ccaf9c72a35fc682d9
Belinda <liangbelinda@gmail.com>
2024-05-08 22:28:57 -0400

    added job zones 4 and 5 pages + footer changes

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Footer() {
+
+    return (
+        <div className="block-gorup">
+            <p className="block-group footer">
+                Copyright © 2023 – CareerSpring. All rights reserved. Registered 501(c)(3), EIN 85-1275392
+            </p>
+
+        </div>
+    )
+}
