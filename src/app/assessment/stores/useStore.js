264bfde2715018b6e531ec7d05a37c9307598a63
davonbl-bl <davonbl@gmail.com>
2024-05-22 20:18:17 -0400

    create a useStore function called urlStore through zustand in the useStore.js file

--- src/app/assessment/stores/useStore.js
+++ src/app/assessment/stores/useStore.js
@@ -1,82 +1,21 @@
 import {create} from 'zustand';
 import {devtools, persist, createJSONStorage} from 'zustand/middleware'
 
-
-// const useStore = create((set) => {
-//     url: 'https://services.onetcenter.org/ws/mnm/interestprofiler/questions',
-//     setUrl: () => set((state) => ({url: state.url})),
-// })
-
-// const urlStore = create((set) => ({
-//     url: 'https://services.onetcenter.org/ws/mnm/interestprofiler/questions',
-//     setUrl: () => set((state) => ({url: state.url}))
-// }))
-
-// const urlStore = create((set) => ({
-//     url: 'https://services.onetcenter.org/ws/mnm/interestprofiler/questions',
-//     setUrl: (updatedURL) => set(() => ({url: updatedURL}))
-// }))
-
-/* This is the one that I used perviously */
-
-// const urlStore = create((set) => ({
-//     url: '',
-//     setUrl: (updatedURL) => set(({url: updatedURL}))
-// }))
-
-
-// const useStore = create((set) => ({
-//     url: "https://services.onetcenter.org/ws/mnm/interestprofiler/questions",
-//     increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-//     removeAllBears: () => set({ bears: 0 }),
-//     updateBears: (newBears) => set({ bears: newBears }),
-//   }))
-
-// this is for the refreshner
-
-// const urlStore = create(
-//   persist(
-//     (set, get) => ({
-//       url: '',
-//       setUrl: (updatedURL) => set({ url: get().updatedURL }),
-//     }),
-//     {
-//       name: 'url-storage', // name of the item in the storage (must be unique)
-//       storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
-//     },
-//   ),
-// )
-
-
-// const urlStore = create(
-//     persist(
-//       (set, get) => ({
-//         url: '',
-//         setUrl: (updatedURL) => set(() => {
-//             const getUrl = updatedURL
-//             return {url: get().updatedURL}
-//         }),
-//       }),
-//       {
-//         name: 'url-storage', // name of the item in the storage (must be unique)
-//         storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
-//       },
-//     ),
-//   )
-
-const urlStore = create(
+  const urlStore = create(
     persist(
-      (set, get) => ({
-        url: '',
-        setUrl: (updatedURL) => set({ url: updatedURL })
+      (set) => ({
+        url: 'https://services.onetcenter.org/ws/mnm/interestprofiler/questions',
+        setUrl: (updatedURL) => {
+          set({ url: updatedURL });
+          console.log('Updated URL:', updatedURL);
+        },
       }),
       {
-        name: 'url-storage', // name of the item in the storage (must be unique)
-        storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
-      },
-    ),
-  )
-
+        name: 'url-storage', // unique name for the storage item
+        storage: createJSONStorage(() => localStorage), // use localStorage for persistence
+      }
+    )
+  );
 
 
 export default urlStore; 
