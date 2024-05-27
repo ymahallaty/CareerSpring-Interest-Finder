fb9cfeeda13f698fbc85b05c377a36264728f480
davonbl-bl <davonbl@gmail.com>
2024-05-27 17:24:14 -0400

    removed commented out code

--- src/app/assessment/stores/useStore.js
+++ src/app/assessment/stores/useStore.js
@@ -1,21 +1,21 @@
 import {create} from 'zustand';
 import {devtools, persist, createJSONStorage} from 'zustand/middleware'
 
-  const urlStore = create(
+  const useStore = create(
     persist(
       (set) => ({
         url: 'https://services.onetcenter.org/ws/mnm/interestprofiler/questions',
         setUrl: (updatedURL) => {
           set({ url: updatedURL });
-        //   console.log('Updated URL:', updatedURL);
+          console.log('Updated URL:', updatedURL);
         },
       }),
       {
-        name: 'url-storage', // unique name for the storage item
-        storage: createJSONStorage(() => localStorage), // use localStorage for persistence
+        name: 'url-storage', // this here is a unique name for the storage item
+        storage: createJSONStorage(() => localStorage), // we use localStorage for persistence
       }
     )
   );
 
 
-export default urlStore; 
+export default useStore; 
