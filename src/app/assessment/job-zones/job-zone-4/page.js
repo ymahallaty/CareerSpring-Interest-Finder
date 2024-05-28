c5311f35fb25d023ee7c3e5031ebd26e9ff8eb6f
Belinda <liangbelinda@gmail.com>
2024-05-28 04:30:50 -0400

    job zones with data fetched from ONET API

--- src/app/assessment/job-zones/job-zone-4/page.js
+++ src/app/assessment/job-zones/job-zone-4/page.js
@@ -1,7 +1,35 @@
+"use client";
+
+import React from "react";
+import { useState,useEffect } from "react";
 import Link from "next/link";
+import axios from "axios";
+import useSWR from "swr";
+
+const fetcher = url => axios.get(url).then(res => res.data);
 
 export default function jobZone4() {
-  return (
+    
+  const [zone_info, setZone_info] = useState([]);
+
+  const [url, setUrl] = useState('https://services.onetcenter.org/ws/mnm/interestprofiler/job_zones');
+
+  const fetchURL = `../../../assessment/api?url=${encodeURIComponent(url)}`;
+
+  const { data, error } = useSWR(fetchURL, fetcher);
+
+  useEffect(() => {
+    if (error) {
+      console.error('Failed to load:', error);
+    }
+
+    if (data) {
+      setZone_info(data.job_zone[3]);
+      console.log(data.job_zone[3]);
+    }
+  }, [data, error]);
+  
+    return (
     <div className="pageDiv">
       <h1 className="titleH1">
         Information On Job Zone 4: High Job Preparation
@@ -12,34 +40,25 @@ export default function jobZone4() {
           <strong>Experience</strong>
         </h2>
         <p>
-          Long term work-related skill, knowledge, or experience is needed for
-          these careers. For example, an accountant must complete four years of
-          college and work several years in the field to be qualified for the
-          job.
+          {zone_info.experience}
         </p>
         <h2>
           <strong>Training</strong>
         </h2>
         <p>
-          Employees in these careers need several years of work-related
-          experience and training. Both on-the-job and classroom job training
-          may be needed.
+        {zone_info.job_training}
         </p>
         <h2>
           <strong>Education</strong>
         </h2>
         <p>
-          Most of these careers need a four-year bachelor’s degree, but some do
-          not.
+        {zone_info.education}
         </p>
         <h2>
           <strong>Examples</strong>
         </h2>
         <p>
-          Many of these careers involve coordinating, supervising, managing, or
-          training others. Examples include real estate brokers, sales managers,
-          database administrators, graphic designers, conservation scientists,
-          art directors, and cost estimators.
+        {zone_info.examples}
         </p>
         <div className="button-container mt-10">
         <Link href="/assessment/job-zones">
