87f550b4adc882dd74b157402633edb902497567
davonbl-bl <davonbl@gmail.com>
2024-06-01 15:49:49 -0400

    created the pageIdStore to convert page_id local state to a global state

---
+++ src/app/assessment/stores/pageIdStore.js
@@ -0,0 +1,21 @@
+import {create} from 'zustand';
+import {persist, createJSONStorage} from 'zustand/middleware'
+
+  const urlStore = create(
+    persist(
+      (set) => ({
+        page_id: '1',
+        setPage_id: (update_pageID) => {
+          set({ page_id: update_pageID });
+          console.log('Updated page_id:', page_id);
+        },
+      }),
+      {
+        name: 'page_id-storage', // this here is a unique name for the storage item
+        storage: createJSONStorage(() => localStorage), // we use localStorage for persistence
+      }
+    )
+  );
+
+
+export default urlStore; 
