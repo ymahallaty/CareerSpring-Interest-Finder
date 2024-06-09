a847b546d815609008d942eb2d54f3f9674bd92e
davonbl-bl <davonbl@gmail.com>
2024-06-09 17:20:11 -0400

    have a default url, get the query strings from the searchParams variable, and evulate that variable in terms of it having query strings or not, if it does then the query strings will be appended to the default url, if not then the default url will remain as it is

--- src/app/assessment/api/route.js
+++ src/app/assessment/api/route.js
@@ -4,12 +4,52 @@ import { NextResponse } from 'next/server'
 
 export const GET = async(req,res) => {
 
-    // const {url} = req.query;
-    // console.log('get the url: ', url)
+    let currentApiUrl = 'https://services.onetcenter.org/ws/mnm/interestprofiler/questions'
+    
+    const searchParams = req.nextUrl.searchParams
+    console.log('get the searchParams: ', [...searchParams].length)
+    // const getQuery = searchParams.get('start')
+    // const getQuery2 = searchParams.get('end')
+    // console.log('get getQuery: ', getQuery)
+    // console.log('get getQuery2: ', getQuery2)
+    if([...searchParams].length){
+    //  console.log('get the searchParams: ', searchParams))
+        // let start = req.query.start;
+        // let end = req.query.end
+        let start = searchParams.get('start');
+        let end = searchParams.get('end')
+        console.log('here is the start: ', start)
+        console.log('here is the end: ', end)
+        currentApiUrl = currentApiUrl + `?start=${start}&end=${end}`
+        console.log('here is the NEW URL: ', currentApiUrl)
+    }
+    
+    else{
+        console.log('no req.nextUrl.searchParams: ', req.nextUrl.searchParams)
+        console.log('here is the req.url: ', req.url)
+        console.log('here is the req.url.search: ', req.url.search)
+    }
+    
+    // const {start, end} = req.query
+    // if(start && end){
+    //     currentApiUrl = currentApiUrl + `?${start}&${end}`
+    // }
+    
 
-    const { searchParams } = new URL(req.url);
-    const url = searchParams.get('url');
+    // if(start && end){
+    //     currentApiUrl = currentApiUrl + `?${start}&${end}`
+    // }
 
+    // const testing = req.query;
+    // console.log('get the value of url2: ', testing)
+
+    // const { searchParams } = new URL(req.url);
+    // console.log('get the value of searchParams: ', searchParams)
+    // const url = searchParams.get('url');
+    // console.log('get the value of url: ', url)
+    // console.log('get the value of requrest.url: ', req.url)
+
+//////////////////////////////////////////// 
     try {
         const options = {
             auth: {
@@ -18,8 +58,13 @@ export const GET = async(req,res) => {
             }
         }
         // const response = await axios.get('https://services.onetcenter.org/ws/mnm/interestprofiler/questions', options)
-        const response = await axios.get(url, options)
-        return NextResponse.json(response.data)        
+        // const response = await axios.get(url, options)
+        const response = await axios.get(currentApiUrl, options)
+        // console.log('here is the response: ', response)
+        console.log('here is the FINAL FINAL URL: ', currentApiUrl )
+        https://services.onetcenter.org/ws/mnm/interestprofiler/questions?start=13&end=24
+        return NextResponse.json(response.data)       
+        // return response.data       
     } catch (error) {
         res.status(500).json({ error: 'Unable to fetch data' })
     }
