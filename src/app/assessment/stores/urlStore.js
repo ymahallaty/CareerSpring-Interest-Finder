2f1c74520f45f47a7aec3e4e2328a807409a2f26
davonbl-bl <davonbl@gmail.com>
2024-06-09 17:17:45 -0400

    changed the inital url value from the url itself to an empty string as it is not needed since the url is store on the route handler

--- src/app/assessment/stores/urlStore.js
+++ src/app/assessment/stores/urlStore.js
@@ -4,7 +4,8 @@ import {persist, createJSONStorage} from 'zustand/middleware'
   const urlStore = create(
     persist(
       (set) => ({
-        url: 'https://services.onetcenter.org/ws/mnm/interestprofiler/questions',
+        // url: 'https://services.onetcenter.org/ws/mnm/interestprofiler/questions',
+        url: '',
         setUrl: (updatedURL) => {
           set({ url: updatedURL });
           // console.log('Updated URL:', updatedURL);
