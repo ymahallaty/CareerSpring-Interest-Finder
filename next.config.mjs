7b8ebbc0eea858dfd064b582e4c8757faa730d48
davonbl-bl <davonbl@gmail.com>
2024-06-10 22:46:03 -0400

    added * at the end of the source key value

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+ 
 const nextConfig = {
     async headers() {
         return [
             {
-                source:"/app/assessment/api/:path",
+                source:"/app/assessment/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, 
