7f6d824ad135d166e3771c1eda310c05d8b4374d
davonbl-bl <davonbl@gmail.com>
2024-06-10 21:35:48 -0400

    revised the source due to the api being nested in the assessment folder

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source:"/api/:path",
+                source:"/assessment/api/:path",
                 headers: [
                     { key: "Access-Control-Allow-Origin", value: "http://localhost:3000"}, 
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
@@ -14,3 +14,4 @@ const nextConfig = {
 };
 
 export default nextConfig;
+src\app\assessment\api
