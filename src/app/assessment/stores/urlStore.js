e4a7cc3f39e7a667b53f002d1086524761239192
davonbl-bl <davonbl@gmail.com>
2024-06-11 03:33:14 -0400

    revert back to the empty string and route paths

--- src/app/assessment/stores/urlStore.js
+++ src/app/assessment/stores/urlStore.js
@@ -8,9 +8,8 @@ import {persist, createJSONStorage} from 'zustand/middleware'
       
       (set) => ({
         // url: `../assessment/api`,
-        // url: 'https://services.onetcenter.org/ws/mnm/interestprofiler/questions',
-        // url: '',
-        url:`http://localhost:3000/assessment/api`,
+        url: '',
+        // url:`http://localhost:3000/assessment/api`,
         setUrl: (updatedURL) => {
           set({ url: updatedURL });
           // console.log('Updated URL:', updatedURL);
