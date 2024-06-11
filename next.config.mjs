add036c6b1921a598d79aa6446eb9ece0344aa38
davonbl-bl <davonbl@gmail.com>
2024-06-10 23:25:40 -0400

    changed the source value

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,7 @@ const nextConfig = {
         return [
           {
             // matching all API routes
-            source: "/api/:path*",
+            source: "/app/assessment/api/:path*",
             headers: [
               { key: "Access-Control-Allow-Credentials", value: "true" },
               { key: "Access-Control-Allow-Origin", value: "*" },
