7e651da6422350311192e096df9548687ab458b4
davonbl-bl <davonbl@gmail.com>
2024-06-11 00:20:04 -0400

    changed the state value to a path

--- src/app/welcome/page.js
+++ src/app/welcome/page.js
@@ -18,9 +18,10 @@ function Page() {
   const {showPageId, defaultPage_id} = pageIDStore()
 
   useEffect(() => {
-    updateURL('http://localhost:3000/assessment/api')
+    // updateURL('http://localhost:3000/assessment/api')
     // updateURL('');
-    // updateURL(`../assessment/api`);
+    updateURL('../assessment/api');
+    
   }, [updateURL]);
 
   function startingSurvey(){
