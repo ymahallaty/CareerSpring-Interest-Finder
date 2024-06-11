77d84f9d9ea7365863489bd269906f81d0fc45d3
davonbl-bl <davonbl@gmail.com>
2024-06-10 22:33:48 -0400

    I pushed the assessment page and not the next config file

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source:"/app/assessment/api/:path",
+                source:"/assessment/api/:path",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" }, 
