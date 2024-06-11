ba45ae8b0aed9eb5f4ab0ab9c660556ba6acc325
davonbl-bl <davonbl@gmail.com>
2024-06-10 21:33:11 -0400

    added an async headers function with values related to cors to ensure the assessment page works on the deployment preview

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source:"/api/:path",
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3000"}, 
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                ]
+            }
+        ]
+    }
+};
 
 export default nextConfig;
