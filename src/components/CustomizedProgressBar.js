625185afbe59cf22db9447c78f8e6dee7bcf2024
dayofthetech <j.alberto.reyes.r90@gmail.com>
2024-06-16 18:19:09 -0400

    progress bar styling

--- src/components/CustomizedProgressBar.js
+++ src/components/CustomizedProgressBar.js
@@ -2,25 +2,35 @@
 import React from 'react';
 import LinearProgress from '@mui/material/LinearProgress';
 import { styled } from '@mui/material/styles';
-
 const Container = styled('div')({
   width: '70%', // Set container width to fill its parent
   height: '40px', // Set container height to a fixed value or adjust as needed
 });
-
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   height: '100%', // Set progress bar height to fill its container
   width: '100%', // Set progress bar width to fill its container
   borderRadius: '30px',
   backgroundColor:'white',
   border:'2px solid black',
-  padding: '20px', 
+
+  '&::before': {
+    content: '""',
+    position: 'absolute',
+    top: '0px',
+    left: '0px',
+    right: '0px',
+    bottom: '0px',
+    border: '10px solid white',
+    borderRadius: '30px',
+    zIndex: 1,
+  },
+
   [`& .MuiLinearProgress-bar`]: {
-    borderRadius: 5,
+    borderRadius: '30px',
+    margin: '9px',
     backgroundColor: '#ff9e1b', // Set background color to yellow
   },
 }));
-
 export default function CustomizedProgressBar({ value }) {
   return (
     <Container>
