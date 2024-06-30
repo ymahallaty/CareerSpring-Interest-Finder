a797d6c4cbf555fbf5495726fccf05196943fcd1
davonbl-bl <davonbl@gmail.com>
2024-06-30 16:33:21 -0400

    commented and deleted some unused code

--- src/app/welcome/page.js
+++ src/app/welcome/page.js
@@ -2,56 +2,8 @@
 
 import React from "react";
 import Link from "next/link";
-import { useEffect } from "react";
-import urlStore from "../assessment/stores/urlStore";
-import renderAnswersStore from "../assessment/stores/renderAnswersStore";
-import pageIDStore from "../assessment/stores/pageIdStore"
-import { useRouter } from 'next/navigation'
 
 function Page() {
-
-  const showURL = urlStore((state) => state.url)
-  const updateURL = urlStore((state) => state.setUrl)
-
-  const setAnswersObject = renderAnswersStore((state) => state.setAnswersObject)
-  const showAnswersObject = renderAnswersStore((state) => state.answersObject)
-
-  const {showPageId, defaultPage_id} = pageIDStore()
-
-  const router = useRouter()
-
-  useEffect(() => {
-    // updateURL('http://localhost:3000/assessment/api')
-    updateURL('');
-    // updateURL('../assessment/api');
-    
-  }, [updateURL]);
-
-  function startingSurvey(){
-    if(showAnswersObject !== ''){
-      setAnswersObject('')
-    }
-    if(showPageId !== 1){
-      defaultPage_id(1)
-    }
-
-    // router.push(`/assessment?page_id=1`)
-    
-  }
-
-  // const env = process.env.NODE_ENV
-  // if(env == "development"){
-  //   // Development-specific code
-  //   console.log('Running in development mode');
-  // }
-  // else if (env == "production"){
-  //   // Production-specific code
-  //   console.log('Running in production mode');
-  // }
-
-
-  // console.log('showURL: ', showURL);
-  // console.log('show page_id number', showPageId)
 
   return (
     <div className="pageDiv">
@@ -75,7 +27,8 @@ function Page() {
       <h1 className="paragraph my-5">Remember, there are no right or wrong answers!</h1>
       <Link href={`/assessment?page_id=1&start=1&end=12`}>
       {/* <Link></Link> */}
-        <button onClick={startingSurvey} className="orangeBrand py-5 text-base leading-7 text-white p-[65px] rounded-md interFont"> Start Career Interest Finder
+      {/* onClick={startingSurvey} */}
+        <button className="orangeBrand py-5 text-base leading-7 text-white p-[65px] rounded-md interFont"> Start Career Interest Finder
         </button>
       </Link>
       <h1 className="paragraph my-4">Taken the Interest Profiler before?</h1>
