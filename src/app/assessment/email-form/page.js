b092cd49944f23f38be328dbe9130834791b714b
davonbl-bl <davonbl@gmail.com>
2024-07-05 16:40:36 -0400

    added error statements in regards to emailJS

--- src/app/assessment/email-form/page.js
+++ src/app/assessment/email-form/page.js
@@ -115,6 +115,9 @@ function EmailForm() {
     } catch (error) {
       if (error.response && error.response.data && error.response.data.error) {
         alert("Error sending data: " + error.response.data.error);
+        console.log('error-response: ', error.response)
+        console.log('error-response: ', error.response.data)
+        console.log('error.response.data.error: ', error.response.data.error)
       } else {
         alert("An error occurred while sending data.");
       }
