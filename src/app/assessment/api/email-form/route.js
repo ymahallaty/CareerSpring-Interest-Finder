3bc562f2c4c16b0f3f8085742b85c5e69b09816d
davonbl-bl <davonbl@gmail.com>
2024-07-06 11:09:04 -0400

    added the private_key with the replace method at the end on the email-form route page file, and took out the comma at the end

--- src/app/assessment/api/email-form/route.js
+++ src/app/assessment/api/email-form/route.js
@@ -36,10 +36,11 @@ export async function POST(req) {
     // Authenticate with Google Sheets API
 
     // private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    // private_key: process.env.GOOGLE_PRIVATE_KEY,
     const auth = new google.auth.GoogleAuth({
       credentials: {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
       },
       scopes: [
         "https://www.googleapis.com/auth/drive",
